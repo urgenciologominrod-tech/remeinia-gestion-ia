c3511ae8058b1e633ebf3b7b4107f7090ffe8a79
urgenciologominrod-tech <urgenciologominrod@gmail.com>
2026-05-12 22:30:09 -0600

    Make React entrypoint explicit and stable

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
 import './App.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const rootElement = document.getElementById('root');
+
+createRoot(rootElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
